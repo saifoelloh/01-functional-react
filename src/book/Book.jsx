2be618f08f751a9efb5988b8c27149620bcea38c
saifoelloh <saifoelloh@gmail.com>
2020-01-12 23:58:34 +0700

    adding axios into project

--- src/book/Book.jsx
+++ src/book/Book.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, {useState, useEffect} from 'react'
+import Axios from 'axios'
 
 function Book(props) {
   const [name, setName] = useState('')
@@ -6,27 +7,55 @@ function Book(props) {
   const [email, setEmail] = useState('')
   const kotak = {
     padding: '50px',
-    backgroundColor: 'lightblue'
+    backgroundColor: 'lightblue',
   }
   const [btn, setBtn] = useState(true)
-  const card = <div style={kotak}>
-            <p>Nama = {name}</p>
-            <p>Umur = {age}</p>
-            <p>Email = {email}</p>
-          </div>
+  const card = (
+    <div style={kotak}>
+      <p>Nama = {name}</p>
+      <p>Umur = {age}</p>
+      <p>Email = {email}</p>
+    </div>
+  )
+
+  useEffect(() => {
+    setInterval(() => {
+      Axios.get('https://jsonplaceholder.typicode.com/users')
+        .then((res) => {
+          const datum = res.data[0]
+          setName(datum.name)
+          setAge(datum.id)
+          setEmail(datum.email)
+        })
+        .catch(console.error)
+    }, 5000)
+  })
 
   return (
     <div>
       <h1>Form | {props.title}</h1>
-      <input type="text" value={name} onChange={e => setName(e.target.value)} placeholder="John Smith" />
-      <input type="number" value={age} onChange={e => setAge(e.target.value)} placeholder="25" />
-      <input type="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="foo@mail.com" />
-      <button onClick={e => setBtn(!btn) }>
-        tekan
-      </button>
-      { btn ? 'foo' : card }
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder="John Smith"
+      />
+      <input
+        type="number"
+        value={age}
+        onChange={(e) => setAge(e.target.value)}
+        placeholder="25"
+      />
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="foo@mail.com"
+      />
+      <button onClick={(e) => setBtn(!btn)}>tekan</button>
+      {btn ? 'foo' : card}
     </div>
   )
 }
 
-export default Book;
+export default Book
