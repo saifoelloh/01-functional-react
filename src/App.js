705b0bc4630b982c0d2bb12015888ece8752d089
saifoelloh <saifoelloh@gmail.com>
2020-01-13 23:37:57 +0700

    minor update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
-import React, {useState} from 'react';
-import Book from './book/Book.jsx';
+import React from 'react'
+import Book from './book/Book.jsx'
 
 function App() {
-  const [title, setTitle] = useState('Buku')
-  const changeHandler = e => {
-    setTitle(e.target.value)
-  }
+  const title = 'User List'
   return (
     <div>
       <h1>Head</h1>
-      <input type="text" onChange={changeHandler} value={title} placeholder="Judul" />
       <Book title={title} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
